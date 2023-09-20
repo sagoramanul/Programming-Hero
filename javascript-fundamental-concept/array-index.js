37ccbfa6c7d45c61fceb4e78a09f0d126cb5fa98
Amanul Islam <sagoramanulislam@gmail.com>
2023-09-21 00:07:20 +0600

    array

---
+++ javascript-fundamental-concept/array-index.js
@@ -0,0 +1,9 @@
+var friendAge = [20, 22, 24,26];
+var sonaliAge = friendAge[2];
+friendAge[2] = 30;
+var positon = friendAge.indexOf(26);
+
+console.log(friendAge);
+console.log(friendAge[3]);
+console.log(sonaliAge);
+console.log(positon);
