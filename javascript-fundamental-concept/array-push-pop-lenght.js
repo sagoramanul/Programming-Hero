1877f91817bcdc10cb60055ac0d14e6330bab840
Amanul Islam <sagoramanulislam@gmail.com>
2023-09-21 00:16:27 +0600

    push-pop & lenght

---
+++ javascript-fundamental-concept/array-push-pop-lenght.js
@@ -0,0 +1,13 @@
+var friendAge = [20, 60, 80, 65, 32, 40];
+console.log(friendAge);
+
+
+
+friendAge.push(24);
+console.log(friendAge);
+
+friendAge.pop();
+console.log(friendAge);
+
+
+console.log(friendAge.length);
