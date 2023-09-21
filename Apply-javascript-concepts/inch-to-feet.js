209046d07415061ee03d5cb58ae997af0a3c3fa3
Amanul Islam <sagoramanulislam@gmail.com>
2023-09-21 13:20:12 +0600

    update

---
+++ Apply-javascript-concepts/inch-to-feet.js
@@ -0,0 +1,20 @@
+var inch = 156;
+var feet = inch/12;
+
+console.log(feet);
+
+console.log("==============================================");
+
+function feetcount (inchi){
+    var feetis = inchi/12;
+
+    return feetis;
+}
+
+var nanarfeet = feetcount(588);
+var nanirfeet = feetcount(256);
+var babarfeet = feetcount(455);
+
+console.log(nanarfeet);
+console.log(nanirfeet);
+console.log(babarfeet);
