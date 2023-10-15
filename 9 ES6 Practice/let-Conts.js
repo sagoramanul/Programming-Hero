c9a60017140c02ba179f62184efea0c2ee379e3b
Amanul Islam <sagoramanulislam@gmail.com>
2023-10-15 22:31:33 +0600

    es6 practice

---
+++ 9 ES6 Practice/let-Conts.js
@@ -0,0 +1,25 @@
+const myName = "Sagor";
+console.log(myName);
+
+
+const numbers = [12,35];
+numbers [1] = 88;
+numbers.push(12);
+console.log(numbers);
+
+const nayok = {name: "Sharukh khan", Phone: 01689056023, }
+console.log(nayok);
+
+
+
+
+let patientName = "Rahim Hossain";
+//patientName= "Rahima Khatun";
+console.log(patientName);
+
+
+let sum = 0;
+for(let i = 0; i<10; i++){
+    sum = sum + i;
+}
+console.log(i);
