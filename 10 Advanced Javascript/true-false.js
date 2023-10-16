3d7bc2d0ae9d4fd9873c4ecd6cc93bc7b2d115af
Amanul Islam <sagoramanulislam@gmail.com>
2023-10-16 13:36:21 +0600

    advanced javascript

---
+++ 10 Advanced Javascript/true-false.js
@@ -0,0 +1,19 @@
+//FALSY:
+//false
+// 0
+// ""
+// NAN
+// nul
+// undefined
+
+//TRUTHY
+// '0', " ", []
+
+
+const age = 4;
+
+if(age > 0){
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
