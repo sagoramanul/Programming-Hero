4c959b932dca76531d23811d2bf1edbfdd72d2d5
Amanul Islam <sagoramanulislam@gmail.com>
2023-10-16 15:57:00 +0600

    Undefined

---
+++ 10 Advanced Javascript/null & undefined.js
@@ -0,0 +1,36 @@
+//==============
+//UNDEFINE
+//==============
+
+// let pakhi;
+
+// //pakhi is undefined
+// console.log(pakhi);
+
+//console.log("==========================");
+
+// function add(num1, num2){
+//     console.log(num1 + num2);
+// }
+
+// const result = add(13,83);
+// console.log(result);
+
+//console.log("==========================");
+
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
+// const result = add(83,7);
+// console.log(result);
+
+//console.log("==========================");
+
+// const student = {name:"Rayhan akond", phone:"01675893097"};
+// console.log(student.id);
+
+
+//==============
+//NULL
+//==============
+
